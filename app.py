6096d4b7ea5b2b833cad30115f9e80a84b402022
pauloqxm <pauloqxm@gmail.com>
2025-07-09 11:44:00 -0300

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.express as px
 # 🔒 Proteção por senha
 def check_password():
     def password_entered():
-        if st.session_state["password"] == "minhasenha123":
+        if st.session_state["password"] == "1234":
             st.session_state["password_correct"] = True
             del st.session_state["password"]
         else:
