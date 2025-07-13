57d526e8b7de60c24bcf96e30c2c412ca6bffedd
pauloqxm <pauloqxm@gmail.com>
2025-07-13 15:10:42 -0300

    15.11 app.py

--- app.py
+++ app.py
@@ -59,8 +59,7 @@ if aba == "Vazões - GRBANABUIU":
             "OpenStreetMap", "Stamen Terrain", "Stamen Toner",
             "CartoDB positron", "CartoDB dark_matter", "Esri Satellite"
         ], index=0)
-        mostrar_acudes = st.checkbox("💧 Exibir Açudes Monitorados no mapa", value=True)
-
+      
     df_filtrado = df.copy()
     if estacoes:
         df_filtrado = df_filtrado[df_filtrado['Reservatório Monitorado'].isin(estacoes)]
