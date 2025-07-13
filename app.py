861126ec572f453832924aacdb957dccf51786bf
pauloqxm <pauloqxm@gmail.com>
2025-07-13 15:21:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -142,13 +142,7 @@ if aba == "Vazões - GRBANABUIU":
             style_function=lambda x: {"color": "darkblue", "weight": 2}
         ).add_to(m)
             
-            folium.GeoJson(
-            geojson_Açudes_Monitorados,
-            name="Açudes Monitorados",
-            tooltip=folium.GeoJsonTooltip(fields=["Name"], aliases=["Trecho:"]),
-            style_function=lambda x: {"color": "darkblue", "weight": 2}
-        ).add_to(m)
-
+     
         for _, row in df_mapa.iterrows():
             popup_info = f"""
             <strong>Reservatório:</strong> {row['Reservatório Monitorado']}<br>
