53c0ea198592849737687697ab39568a609040c2
pauloqxm <pauloqxm@gmail.com>
2025-07-14 14:37:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ with st.sidebar:
         st.markdown("---")
         st.image("https://i.ibb.co/tpQrmPb0/csbh.png", use_column_width=True)
         st.markdown("""
-        <div style="text-align: center; font-size: 12px; color: #666;">
+        <div style="text-align: top; font-size: 12px; color: #666;">
             © 2023 Companhia de Gestão dos Recursos Hídricos
         </div>
         """, unsafe_allow_html=True)
