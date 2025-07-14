7398bc9af71c3ac296899ff1e90eb4471fcb2881
pauloqxm <pauloqxm@gmail.com>
2025-07-14 18:16:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,52 +25,65 @@ with open("poligno_municipios.geojson", "r", encoding="utf-8") as f:
 
 st.markdown("""
     <style>
-    <style>
-    [data-testid="stSidebar"] {
-        background-color: #e0f0ff;
-        position: relative;
-    }
-   [data-testid="stSidebar"]::after {
-        content: "";
-        position: fixed;
-        bottom: 60px;
-        left: 0;
-        width: 240px;
-        height: 50px;
-        background-image: url('https://i.ibb.co/tpQrmPb0/csbh.png');
-        background-repeat: no-repeat;
-        background-size: contain;
-        background-position: center;
-        z-index: 999;
-    }
-    </style>
-    </style>
-""", unsafe_allow_html=True)
-st.set_page_config(page_title="Dashboard Vazões", layout="wide")
-
-st.markdown("""
-    <style>
     .fixed-header {
         position: fixed;
         top: 0;
-        left: 0;
+        left: 260px;
         right: 0;
+        height: 70px;
         z-index: 1000;
         background-color: #e0f0ff;
+        border-bottom: 2px solid #ccc;
         display: flex;
+        justify-content: center;
         align-items: center;
         gap: 12px;
         padding: 10px 20px;
-        border-bottom: 2px solid #ccc;
+        box-sizing: border-box;
     }
+
     .stApp {
         padding-top: 80px;
+    }
+
+    @media screen and (max-width: 768px) {
+        .fixed-header {
+            left: 0;
+            width: 100vw;
+            justify-content: center;
+            padding-left: 10px;
+            padding-right: 10px;
+        }
     }
     </style>
     <div class="fixed-header">
         <img src="https://i.ibb.co/r2FRGkmB/cogerh-logo.png" alt="Logo COGERH" style="height: 50px;">
         <h2 style="margin: 0; color: #003366;">Operação 2025.2</h2>
     </div>
+""", unsafe_allow_html=True)
+
+
+st.markdown("""
+    <style>
+    [data-testid="stSidebar"]::after {
+        content: "GRBANABUIU";
+        position: absolute;
+        bottom: 10px;
+        left: 0;
+        width: 100%;
+        height: 70px;
+        background-image: url('https://i.ibb.co/tpQrmPb0/csbh.png');
+        background-repeat: no-repeat;
+        background-position: center top;
+        background-size: 50px 50px;
+        padding-top: 60px;
+        text-align: center;
+        font-size: 14px;
+        font-weight: bold;
+        color: #003366;
+        pointer-events: none;
+    }
+    </style>
 """, unsafe_allow_html=True)
 
 with st.sidebar:
