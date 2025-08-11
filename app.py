71413fa344916efad07f54fe4e15a499510cbf6e
pauloqxm <pauloqxm@gmail.com>
2025-08-11 19:36:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ with tab1:
             st.info("Sem dados para média mensal.")
 
     with gtab2:
-    if not df_filtrado.empty and df_filtrado['Reservatório Monitorado'].nunique() > 0:
+        if not df_filtrado.empty and df_filtrado['Reservatório Monitorado'].nunique() > 0:
         yconv, sufx = convert_vazao(df_filtrado['Vazão Operada'], unidade_sel)
         df_box = df_filtrado.copy()
         df_box['Vazão (conv)'] = yconv
