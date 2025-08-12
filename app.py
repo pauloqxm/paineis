203021b29675c04ad22afb44133a2e5dd195bca7
pauloqxm <pauloqxm@gmail.com>
2025-08-11 23:46:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ with tab1:
     color: #006400 !important;
     text-align: center !important;
     margin: 8px 0 10px 0 !important;
-    padding: 8px 22px !important; /* Retângulo mais estreito */
+    padding: 0px 22px !important; /* Retângulo mais estreito */
     position: relative !important;
     display: flex !important;
     align-items: center !important;
