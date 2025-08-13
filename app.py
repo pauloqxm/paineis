e892a3dbc822111c3c57f363c202026db241097c
pauloqxm <pauloqxm@gmail.com>
2025-08-13 18:23:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -698,7 +698,7 @@ with tab2:
     }
 
     # Inicializa o mapa com zoom mais próximo (aumentei o zoom_start para 9)
-    m2 = folium.Map(location=[-5.2, -39.2], zoom_start=9.5, tiles=None)
+    m2 = folium.Map(location=[-5.2, -39.2], zoom_start=9, tiles=None)
 
     # Adiciona a camada da bacia primeiro para calcular o bounds
     bacia_layer = folium.GeoJson(geojson_bacia, 
