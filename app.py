fa277b5b2cf7ea805dca2c8013befffab1b2e67e
pauloqxm <pauloqxm@gmail.com>
2025-08-13 18:59:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ st.markdown(f"""
         align-items: center;
         gap: 30px;
         flex-wrap: wrap;
-        margin-top: -40px;  /* Espaço para aparecer abaixo da barra fixa */
+        margin-top: -20px;  /* Espaço para aparecer abaixo da barra fixa */
         border-bottom: 3px solid #b6b8ba;
         z-index: 1;  /* Mais baixo que o header fixo */
     }}
