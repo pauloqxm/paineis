21e6a60f2144f4082953e93e1f7d83fdc953121e
pauloqxm <pauloqxm@gmail.com>
2025-08-13 21:35:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -910,7 +910,7 @@ with tab2:
     folium.LayerControl(collapsed=True, position='topright').add_to(m2)
     
     # Exibir mapa
-    folium_static(m2, width=900)
+    folium_static(m2, width=100%)
 
 #================PÁGINA > DOCUMENTOS OFICIAS==================
 
